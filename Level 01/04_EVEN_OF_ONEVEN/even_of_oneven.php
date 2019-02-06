d9d593a6e271b3c530c097ede77557485b0b0d17
timothy <timothy@bit-academy.nl>
2019-02-06 16:40:59 +0100

    Opdracht 4 af bijgewerkt

--- Level 01/04_EVEN_OF_ONEVEN/even_of_oneven.php
+++ Level 01/04_EVEN_OF_ONEVEN/even_of_oneven.php
@@ -7,21 +7,20 @@
  */
 while (true) {
 
-    $getal1 = readline(" Please enter a number ");
+    $getal1 = readline("Please enter a number ");
     if (is_numeric($getal1)) {
         if (($getal1 % 2) == 1) {
-            echo "$getal1 is odd.";
-
+            echo "$getal1 is odd.\n";
         }
         if (($getal1 % 2) == 0) {
-            echo "$getal1 is even.";
+            echo "$getal1 is even.\n";
 
         }
     } elseif ($getal1 == "exit") {
-        echo "Sorry to see you go!";
+        echo "Sorry to see you go!\n";
         exit;
     } else {
-        echo "dit is geen goed getal";
+        echo "dit is geen goed getal\n";
 
     }
 }
