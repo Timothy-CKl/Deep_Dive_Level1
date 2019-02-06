4830cc053153107798f3b5da2af5677a31e05546
timothy <timothy@bit-academy.nl>
2019-02-06 13:38:09 +0100

    opdracht 2 af

---
+++ Level 01/02_ZEG_HOI_TERUG/groet.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: timot
+ * Date: 2/6/2019
+ * Time: 1:22 PM
+ */
+echo "hoe gaat uw dag tot nu toe ?";
+$gebruiker = readline(" het gaat ");
+echo "uw dag gaat $gebruiker";
+
+?>
